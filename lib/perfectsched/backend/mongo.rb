ab618323abf81701b51c16e8b8139be085430482
Stephane Bellity <sbellity@gmail.com>
2019-09-13 16:27:00 +0200

    Passing perfectsched logger to Mongo client

--- lib/perfectsched/backend/mongo.rb
+++ lib/perfectsched/backend/mongo.rb
@@ -35,7 +35,8 @@ module PerfectSched
           raise ConfigError, "url option is required for the mongo backend"
         end
 
-        @mongo = Mongo::Client.new(url, config[:mongo_client] || {})
+        mongo_config = { logger: config[:logger] }.merge(config[:mongo_client] || {})
+        @mongo = Mongo::Client.new(url, mongo_config)
         @db = @mongo.database
         @collection = @db[config[:collection] || "perfectsched"]
         @mutex = Mutex.new
